a620a5ba336f6a4dcf1c445dfe9976f177eefb06
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2020-11-08 20:04:09 +0600

    auto sign in using facebook

--- adata.py
+++ adata.py
@@ -1,27 +1,107 @@
 from requests import Session
+from hashlib import sha1
+from base64 import b64decode
+from json import loads
+from time import time
+from re import findall
 import os
 
 WARN_DATA_LOW = 512.0
+AIRTEL_PUBLIC_FACEBOOK_APP_ID = 688591131546006
+OAUTH_URL = 'https://web.facebook.com/v2.3/dialog/oauth'                                            #GET
+GRAPH_URL = 'https://graph.facebook.com/v2.3/me'                                                    #GET
+AIRTEL_LOGIN_DASHBOARD_URL = 'https://www.bd.airtel.com/en/auth/login?redirectTo=/en/dashboard'     #GET
+AIRTEL_LOGIN_URL = 'https://api.bd.airtel.com/v1/account/login/facebook'                            #POST
 
 session = None
 data_warn = False
+airtel_auth_session = None
 
 def init_session():
     global session
     if(session == None):
         session = Session()
-        session.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/82.0',
-            'Authorization': 'Bearer ' + os.environ['AIRTEL_AUTH_SESSION']
-        }
+        fb_cookies = loads(b64decode(os.environ['FB_COOKIES_B64'].encode()))
+        for c in fb_cookies:
+            session.cookies.set(c, fb_cookies[c])
+        session.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/82.0'}
 
-def get_raw_res():
+def genarate_token():
     try:
         init_session()
-        res = session.get('https://api.bd.airtel.com/v1/account/dashboard/internet')
+        oauth_params = {
+            'app_id': AIRTEL_PUBLIC_FACEBOOK_APP_ID,
+            'auth_type': None,
+            'cbt': int(time() * 1000),
+            'channel_url': 'https://staticxx.facebook.com/x/connect/xd_arbiter/?version=46#cb=f1dd5e86ec40b7&domain=www.bd.airtel.com&origin=https%3A%2F%2Fwww.bd.airtel.com%2Ff11dcc402fa7cb8&relation=opener',
+            'client_id': AIRTEL_PUBLIC_FACEBOOK_APP_ID,
+            'display':'popup',
+            'domain':'www.bd.airtel.com',
+            'e2e': '{}',
+            'fallback_redirect_uri': AIRTEL_LOGIN_DASHBOARD_URL,
+            'locale':'en_US',
+            'logger_id': sha1(str(time()).encode()).hexdigest()[:15],
+            'origin': 1,
+            'redirect_uri': 'https://staticxx.facebook.com/x/connect/xd_arbiter/?version=46#cb=f14672d502b76f8&domain=www.bd.airtel.com&origin=https%3A%2F%2Fwww.bd.airtel.com%2Ff11dcc402fa7cb8&relation=opener&frame=f4acbf7735c228',
+            'response_type':'token,signed_request,graph_domain',
+            'return_scopes': False,
+            'scope':'public_profile,email',
+            'sdk':'joey',
+            'version':'v2.3',
+            '_rdc':1,
+            '_rdr': None
+        }
+        
+        oauth_response = session.get(OAUTH_URL, params=oauth_params).text
+        access_token= findall('access_token=([^&]+)', oauth_response).pop()
+
+        graph_params = {
+            'access_token': access_token,
+            'fields':'name,email,picture',
+            'locale':'en_US',
+            'method':'get',
+            'pretty': 0,
+            'sdk':'joey',
+            'suppress_http_code': 1
+        }
+
+        graph_response = session.get(GRAPH_URL, params=graph_params).json()
+
+        airtel_json = {
+            'provider_access_token': access_token,
+            'provider_id': graph_response.get('id'),
+            'name': graph_response.get('name'),
+            'profile_picture': graph_response.get('picture').get('data').get('url'),
+            'email':graph_response.get('email'),
+            'provider':'facebook'
+        }
+
+        airtel_website_session =  session.get(AIRTEL_LOGIN_DASHBOARD_URL).cookies['airtel_website_session']
+
+        airtel_headers = {
+            'X-CSRF-TOKEN': airtel_website_session,
+            'Authorization':'Bearer ' + airtel_website_session
+        }
+
+        token = session.post(AIRTEL_LOGIN_URL, headers=airtel_headers, json=airtel_json).json().get('data').get('token')
+        return token
+    except:
+        return
+
+def get_raw_res(retry=True):
+    global airtel_auth_session
+    try:
+        init_session()
+        headers = {'Authorization': 'Bearer ' + airtel_auth_session}
+        res = session.get('https://api.bd.airtel.com/v1/account/dashboard/internet',headers=headers)
         return res.json()['data']['internetInfo']
     except:
-        print('Someting went wrong.')
+        if(retry):
+            airtel_auth_session = genarate_token()
+            return get_raw_res(False)
+        else:
+            print('Someting went wrong.')
+            return
 
 def format_msg(info):
     try:
@@ -57,7 +137,6 @@ def get_detail_text(res=None):
     if(res == None):
         return 'Someting went wrong. Maybe logged out.'
     return res
-
 
 def do_corn():
     global data_warn
---
+++ cookie_graber.py
@@ -0,0 +1,12 @@
+# This file will be run on your local machine to extract cookies from Firefox
+
+import json
+import base64
+import browser_cookie3
+
+d = dict()
+
+for c in browser_cookie3.Firefox(domain_name='facebook.com').load():
+    d[c.name] =  c.value
+
+print(base64.b64encode(json.dumps(d).encode()).decode())
